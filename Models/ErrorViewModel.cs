a2990581e5f4a670ce70dd16f500210b1098eafd
hung hoang <hung.xamk@gmail.com>
2022-10-05 12:01:14 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokeRank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokeRank.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+        public string JokeQuestioin { get; set; }
+        public int JokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
